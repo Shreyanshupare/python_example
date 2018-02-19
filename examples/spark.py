7e9e2ccb61128af63fc905246f68aa487498be5e
Shreyanshu <shreyanshupare1994@gmail.com>
2018-02-19 10:16:55 +0530

    Update spark.py

--- examples/spark.py
+++ examples/spark.py
@@ -5,7 +5,7 @@ from random import random
 from operator import add
 import pyspark
 from pyspark import SparkConf, SparkContext
-sc = pyspark.SparkContext("spark://192.168.1.68:7077", "job")
+sc = pyspark.SparkContext("spark://192.168.xx.xx:7077", "job")
 
 lines = sc.parallelize(["pandas", "i like pandas"])
 print(lines.collect())
